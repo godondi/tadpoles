d7c2c8a75d199c1f4da49fc10c76ace81160c4a4
devonkennedy1005 <devonkennedy1005@gmail.com>
2026-08-20 10:53:56 -0400

    Adding starter skeleton + Hello World

---
+++ src/main/java/com/neueda/leap/Main.java
@@ -0,0 +1,7 @@
+package com.neueda.leap;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world! From the firm's Sprint 1 project skeleton made by The Tadpoles!");
+    }
+}
